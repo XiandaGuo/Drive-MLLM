beea26e3d04602a19e3da189a7725003aba8bdd2
ruijun <929096570@qq.com>
2024-08-28 01:14:41 +0800

    Add get_MLLM_output.py script and related files, update README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Benchmark-For-MLLM
+
+
+
+### Script
+
+Script name: `get_MLLM_output.py`
+
+Overview: This script utilizes OpenAI's MLLM model to generate responses based on a given `PROMPT_FILE` and images from a specified Hugging Face dataset (`HF_DATASET`). The responses are then saved in a JSON file (`SAVE_JSON_FILE`).
+
+Implementation: The script reads images from the specified Hugging Face dataset in PIL format, converts them to base64 encoding, and then uses OpenAI's API to generate responses.
+
+Tips：
+
+Before using the script, ensure the following:
+
+- `MODEL` is correctly specified.
+- `MAX_TOKENS` is set to a sufficient value.
+- `PROMPT_FILE` points to a valid prompt file.
+- `HF_DATASET` corresponds to the correct dataset.
+- `SAVE_JSON_FILE` is the correct path for saving the output.
+
+To perform small-batch testing, adjust the `dataset_num` parameter accordingly.
---
+++ get_MLLM_output.py
@@ -0,0 +1,78 @@
+from openai import OpenAI
+from datasets import load_dataset
+import io
+import base64
+import json
+
+
+def pil2base64(pil_image):
+    """Converts a PIL image object to a base64-encoded string."""
+    binary_stream = io.BytesIO()
+    pil_image.save(binary_stream, format="PNG")
+    binary_data = binary_stream.getvalue()
+    return base64.b64encode(binary_data).decode('utf-8')
+
+
+if __name__ == "__main__":
+
+    client = OpenAI()
+    MODEL = "gpt-4o"
+    MAX_TOKENS = 300
+
+    PROMPT_FILE = "./eval_prompt.txt"
+    HF_DATASET = "bonbon-rj/MLLM_eval_dataset"
+    SAVE_JSON_FILE = "./mllm_output.json"
+
+    # get prompt
+    object_type = "vehicle"
+    with open(PROMPT_FILE) as f:
+        eval_prompt = (f.read()).format(object_type)
+    text_input = eval_prompt
+    print(f"{'=' * 30}Eval prompt{'=' * 30}")
+    print(eval_prompt)
+
+    # get dataset
+    print(f"{'=' * 30}Get dataset{'=' * 30}")
+    print("This will take a long time.")
+    dataset = load_dataset(HF_DATASET)
+    dataset_num = len(dataset['train'])  # len(dataset['train'])
+
+    # get output
+    print(f"{'=' * 30}Get MLLM output{'=' * 30}")
+    mllm_outputs = []
+    for pic_index in range(dataset_num):
+        print(f"Handing: {pic_index}")
+        sample = dataset['train'][pic_index]
+        image = sample['image']
+
+        image_base64 = pil2base64(image)
+        image_url = f"data:image/jpeg;base64,{image_base64}"
+
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": text_input
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": image_url, },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=MAX_TOKENS,
+        )
+
+        mllm_output = response.choices[0].message.content
+        print(mllm_output)
+        print()
+        mllm_outputs.append(mllm_output)
+
+    # save
+    with open(SAVE_JSON_FILE, 'w') as json_file:
+        json.dump(mllm_outputs, json_file, indent=4)
